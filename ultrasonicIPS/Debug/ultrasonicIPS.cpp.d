40bc94ee0445b690ca03a24bfe62aa8d3098e4ee
ScpCode <1.scpschool.1@gmail.com>
2022-07-29 15:28:38 -0400

    Actually Added Last Commit

---
+++ ultrasonicIPS/Debug/ultrasonicIPS.cpp.d
@@ -0,0 +1,13 @@
+C:\Users\vlrus\AppData\Local\Temp\VMBuilds\ultrasonicIPS\uno\Debug\ultrasonicIPS.cpp.o: \
+ C:\Users\vlrus\AppData\Local\Temp\VMBuilds\ultrasonicIPS\uno\Debug\ultrasonicIPS.cpp \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ D:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Programs\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h
